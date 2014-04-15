d9fb884cfd3668cfa7a6c76ef06517cbaf182064
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 04:48:43 +0530

    cleaning constants

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
 """A simple webapp2 server."""
 import webapp2
-import tweepy
+import keys
+#import tweepy
 
 class MainPage(webapp2.RequestHandler):
 
@@ -9,20 +10,15 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('WEBSITE UNDER MAINTENANCE :P XD \n')
         
-        consumer_key="zrDvgegV0K1lBLIxukR0A"
-        consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw"
-        access_token="1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz"
-        access_token_secret="Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj"
-        #
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
+        #auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+        #auth.set_access_token(keys.access_token, keys.access_token_secret)
         
-        self.response.write('Toekn set\n')
-        api = tweepy.API(auth)
-        user = api.me()
+        #self.response.write('Toekn set\n')
+        #api = tweepy.API(auth)
+        #user = api.me()
 
         #
-        self.response.write(str(user.id))
+        #self.response.write(str(user.id))
         #l=streamer.stream_func()
         #self.response.write(l)
         #counter.main()
---
+++ keys.py
@@ -0,0 +1,4 @@
+consumer_key = 'zrDvgegV0K1lBLIxukR0A'
+consumer_secret = 'dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw'
+access_token = '1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz'
+access_token_secret = 'Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj' 

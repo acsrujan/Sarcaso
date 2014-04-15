81661ccc817858ea799d79ee18bc47275c6833e2
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 01:56:27 +0530

    hello world cleaned

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,6 @@
 """A simple webapp2 server."""
 
-<<<<<<< HEAD
 import streamer
-=======
-#import counter
->>>>>>> c789b19b6c36b7781196ddd0bc279bf9cf87d844
 import webapp2
 
 
@@ -14,12 +10,9 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('WEBSITE UNDER MAINTENANCE :P XD')
-<<<<<<< HEAD
         l=streamer.stream_func()
         self.response.write(l)
-=======
         #counter.main()
->>>>>>> c789b19b6c36b7781196ddd0bc279bf9cf87d844
 
 
 application = webapp2.WSGIApplication([

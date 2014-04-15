1c93464e14c189d37301527105ba98a1d8db4e6f
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 02:01:00 +0530

    testing REST

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,7 @@
 """A simple webapp2 server."""
 
-import streamer
 import webapp2
+import tweepy
 
 
 
@@ -10,8 +10,22 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('WEBSITE UNDER MAINTENANCE :P XD')
-        l=streamer.stream_func()
-        self.response.write(l)
+        
+        consumer_key="zrDvgegV0K1lBLIxukR0A"
+        consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw
+        access_token="1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz"
+        access_token_secret="Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj"
+        
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        
+        api = tweepy.API(auth)
+        user = api.me()
+        
+        self.response.write(str(user.id))
+        
+        #l=streamer.stream_func()
+        #self.response.write(l)
         #counter.main()
 
 

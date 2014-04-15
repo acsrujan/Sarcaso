793dccd961b5f035f3669339508603718c8eb0b4
nitinsutrave <nitinsutrave@gmail.com>
2014-04-15 17:25:59 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -10,7 +10,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('WEBSITE UNDER MAINTENANCE :P XD')
-        counter.main()
+        #counter.main()
 
 
 application = webapp2.WSGIApplication([

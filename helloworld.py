959ae620abbca4541b7d67850e1d006303f12491
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 03:59:55 +0530

    test 2

--- helloworld.py
+++ helloworld.py
@@ -14,9 +14,8 @@ class MainPage(webapp2.RequestHandler):
         
         self.response.write(module.constant)
         
-        """
         consumer_key="zrDvgegV0K1lBLIxukR0A"
-        consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw
+        consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw"
         access_token="1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz"
         access_token_secret="Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj"
         
@@ -27,7 +26,6 @@ class MainPage(webapp2.RequestHandler):
         user = api.me()
         
         self.response.write(str(user.id))
-        """
         #l=streamer.stream_func()
         #self.response.write(l)
         #counter.main()

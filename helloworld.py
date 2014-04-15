7d8a41f13f50ae0725bbd1982321eb6141ce8198
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 04:22:56 +0530

    cleanup

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,6 @@
 """A simple webapp2 server."""
 import webapp2
-import module
-import auth
+import tweepy2
 
 class MainPage(webapp2.RequestHandler):
 
@@ -10,20 +9,20 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('WEBSITE UNDER MAINTENANCE :P XD \n')
         
-        self.response.write(module.constant)
+        consumer_key="zrDvgegV0K1lBLIxukR0A"
+        consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw"
+        access_token="1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz"
+        access_token_secret="Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj"
+        #
+        auth = tweepy2.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
         
-        #consumer_key="zrDvgegV0K1lBLIxukR0A"
-        #consumer_secret="dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw"
-        #access_token="1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz"
-        #access_token_secret="Qh3Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj"
+        self.response.write('Toekn set\n')
+        api = tweepy2.API(auth)
+        user = api.me()
+
         #
-        #auth = OAuthHandler(consumer_key, consumer_secret)
-        #auth.set_access_token(access_token, access_token_secret)
-        #
-        #api = tweepy.API(auth)
-        #user = api.me()
-        #
-        #self.response.write(str(user.id))
+        self.response.write(str(user.id))
         #l=streamer.stream_func()
         #self.response.write(l)
         #counter.main()

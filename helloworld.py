66d378c0c6a7467874b352a8fb829fcefa2709f0
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 19:27:06 +0530

    for loop check 9

--- helloworld.py
+++ helloworld.py
@@ -30,9 +30,9 @@ class MainPage(webapp2.RequestHandler):
 	self.response.write('\n\n')
 
 	self.response.write(str(sutte_timeline[0].text))
-	#for t in sutte_timeline:
+	for t in sutte_timeline:
 	#    self.response.write(new_line)
-	#    self.response.write(str(t.text))
+	    self.response.write(str(t.text))
 	     
         #text = "" 
         #for tweet in sutte_timeline:

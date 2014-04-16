d70e152ccf29bbefe744b0ba15f90a7b6688ebe1
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 17:26:09 +0530

    test printing tweets

--- helloworld.py
+++ helloworld.py
@@ -24,9 +24,9 @@ class MainPage(webapp2.RequestHandler):
 	
 	sutte_timeline = api.home_timeline()
         self.response.write('recieved home_timeline') 
-	#for t in sutte_timeline:
-	#	self.response.write(str(t.id) + '\n' +  str(t.screen_name) + '\n'  + str(t.text))
-	#	self.response.write('\n')
+	for t in sutte_timeline:
+            self.response.write('\n'+ str(t.id) + '\n' +  str(t.screen_name) + '\n'  + str(t.text))
+            self.response.write('\n')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),

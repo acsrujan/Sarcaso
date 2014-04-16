730a8f2c9e08ff82eb16929064bed81399aa1a10
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 17:24:10 +0530

    adding debugging statement

--- helloworld.py
+++ helloworld.py
@@ -23,7 +23,7 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('\n')
 	
 	sutte_timeline = api.home_timeline()
-        
+        self.response.write('recieved home_timeline') 
 	#for t in sutte_timeline:
 	#	self.response.write(str(t.id) + '\n' +  str(t.screen_name) + '\n'  + str(t.text))
 	#	self.response.write('\n')

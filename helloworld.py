67238f936ae164f69aae476315d4a30efa96ecb8
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 21:16:56 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -33,7 +33,7 @@ class MainPage(webapp2.RequestHandler):
 	#self.response.write(str(sutte_timeline[2].text))
 	self.response.write(str(sutte_timeline[3].text))
 	self.response.write(str(sutte_timeline[19].text))
-	#self.response.write(str(sutte_timeline[5].text))
+	self.response.write(str(sutte_timeline[5].text))
 	#self.response.write(str(sutte_timeline[6].text))
 	i=0
 	feed_length = len(sutte_timeline)

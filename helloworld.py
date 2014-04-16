bcd4b07b6597f3608b4c9b36c55e434d245e0f74
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 20:04:44 +0530

    tweet array check 4

--- helloworld.py
+++ helloworld.py
@@ -32,7 +32,7 @@ class MainPage(webapp2.RequestHandler):
 	self.response.write(str(sutte_timeline[1].text))
 	self.response.write(str(sutte_timeline[2].text))
 	self.response.write(str(sutte_timeline[3].text))
-	self.response.write(str(sutte_timeline[4].text))
+	#self.response.write(str(sutte_timeline[4].text))
 	#self.response.write(str(sutte_timeline[5].text))
 	#self.response.write(str(sutte_timeline[6].text))
 	i=0

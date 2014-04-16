558b2fb930686499cdc3db6503ea1be784258dfe
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 16:43:44 +0530

    test 20

--- helloworld.py
+++ helloworld.py
@@ -8,18 +8,6 @@ import webapp2
 
 text = '<title>sarcaso</title><head>Website under maintenance<br></head>'
 
-class StdOutListener(StreamListener):
-    """ A listener handles tweets are the received from the stream.
-This is a basic listener that just prints received tweets to stdout.
-
-"""
-    def on_data(self, data):
-        self.response.write(str(data))
-        return True
-
-    def on_error(self, status):
-        self.response.write(str(status))
-
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
@@ -28,23 +16,18 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(text)
 	    self.response.write('\n')
         
-        l = StdOutListener()
-        
         auth = OAuthHandler(keys.consumer_key, keys.consumer_secret)
         auth.set_access_token(keys.access_token, keys.access_token_secret)
         
-        '''
         api = tweepy2.API(auth)
         user = api.me()
         
         self.response.write(str(user.id))
         self.response.write('\n')
-        '''
         
-        stream = Stream(auth, l)
-        stream.filter(track=['election'])
+        '''
         #counter.main()
-
+        '''
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),

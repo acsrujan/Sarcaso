8ee18db3d1cba3bc5d93ead858bfd0380c167975
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 17:59:08 +0530

    string concat

--- helloworld.py
+++ helloworld.py
@@ -23,10 +23,10 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('\n')
 	sutte_timeline = api.home_timeline()
         self.response.write('recieved home_timeline\n') 
-        
+        text = None 
         self.response.write(str(len(sutte_timeline))) 
-        #for t in sutte_timeline:
-        #    text = '\n'+ str(t.id) + '\t' +  str(t.screen_name) + '\t'  + str(t.text)
+        for t in sutte_timeline:
+            text = text + '\n'+ str(t.id) + '\t' +  str(t.screen_name) + '\t'  + str(t.text)
         
         #self.response.write(text) 
 

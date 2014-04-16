023131542c4b60cc9692ed7f43e2244b05adee20
nitinsutrave <nitinsutrave@gmail.com>
2014-04-16 17:21:37 +0530

    reverting changes

--- helloworld.py
+++ helloworld.py
@@ -22,14 +22,11 @@ class MainPage(webapp2.RequestHandler):
         self.response.write(str(user.id))
         self.response.write('\n')
 	
-	sutte_timeline = api.home_timeline()
-	
-	for t in sutte_timeline:
-		self.response.write(str(t.id) + '\n' +  str(t.screen_name) + '\n'  + str(t.text))
-		self.response.write('\n')
-        '''
-        #counter.main()
-        '''
+	#sutte_timeline = api.home_timeline()
+        
+	#for t in sutte_timeline:
+	#	self.response.write(str(t.id) + '\n' +  str(t.screen_name) + '\n'  + str(t.text))
+	#	self.response.write('\n')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),

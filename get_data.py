9fdf5282118cc1c6f655abafb7bc3dd150ced3fb
nitinsutrave <nitinsutrave@gmail.com>
2014-05-01 04:10:06 +0530

    stating database structure

--- get_data.py
+++ get_data.py
@@ -4,21 +4,26 @@ import time
 import re
 import sys
 
+#consumer_key = 'zrDvgegV0K1lBLIxukR0A'
+#consumer_secret = 'dM2BpTPoEvFBZ9oVVNSGUi5j1T6JQcCcE4At7TeNmw'
+#access_token = '1270521626-CuXFTkLQ58yZbEj2memLErALqMcRbeVp4j8izxz'
+#access_token_secret = 'Qh5Z620M5ttqs3QTE5LNFCRadNmA9XabfakNkbuKMJzdj'
+
 consumer_key = "MYqFUyn5NbEF1crszsg"
 consumer_secret = "ap1cOd6EFbsBRiGthTXrM0M6fN2RCPV0lYFoX1Ys"
 
 access_token = "39950129-0tpjcB6dVKSalWLuBhE4uMsVryR5MBAntJWsudVuG"
-access_token_secret = "FLn2dTqPRtDC1sF4tMRzXpoWXby0Na97OW3WA2hGEnME7"
+access_token_secret = "FLn0dTqPRtDC1sF4tMRzXpoWXby0Na97OW3WA2hGEnME7"
 
 auth = tweepy2.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy2.API(auth_handler=auth, proxy_url='172.31.16.10:8080')
-user = api.me()
 scr_name=sys.argv[1]
 print scr_name
-tweet_list = api.user_timeline(scr_name, count=200)
+user = api.me()
 
-tweets = [tweet.text.encode('ascii', 'ignore') for tweet in tweet_list]
-for t in tweets:
-    print t
+tweet_list = api.user_timeline(scr_name, count=200)
+f = open(r'user_tweet_history', 'w')
+for tweet in tweet_list:
+    f.write(re.sub('\n+', ' ', tweet.text.encode('ascii','ignore'))+"\n")

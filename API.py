c4250acbffffd6644e7279f4060cd26c876928fd
nitinsutrave <nitinsutrave@gmail.com>
2014-05-01 06:23:56 +0530

    data update 1

--- API.py
+++ API.py
@@ -6,11 +6,14 @@ user1.set_screen_name('SuTTe')
 
 print user1.get_screen_name()
 
-f = open('tweet_data','r')
+f = open('user_data/history/history_user_1','r')
 
 s=[]
 
 for line in f:
     s.append(str(line))
 
-print user1.create_subject_list(s)
+f2 = open('user_data/opinions/opinion_user_1','w')
+a =  user1.create_subject_list(s)
+f2.write(str(a))
+print a

1a971dc56daa5e83650525dd73698e5005d5b13b
nitinsutrave <nitinsutrave@gmail.com>
2014-05-01 05:58:47 +0530

    history_user_1 added

---
+++ main.py
@@ -0,0 +1,15 @@
+import polarity
+
+#initialized score database
+polarity_scores = polarity.get_scores()
+
+#initialize user history
+
+#input test data
+
+#analyse test data
+    #lexical analysis
+
+    #like/dislike analysis
+
+#output answer
